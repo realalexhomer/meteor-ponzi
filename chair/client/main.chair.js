6b175993ddf2ef7cf0e03db4f9dca35a3ea083ab
realalexhomer <realalexhomer@gmail.com>
2015-05-23 12:33:50 -0400

    finish adding nav thing

--- chair/client/main.chair.js
+++ chair/client/main.chair.js
@@ -2,18 +2,31 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  // Template.hello.helpers({
+  //   counter: function () {
+  //     return Session.get('counter');
+  //   }
+  // });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  // Template.navbar.helpers({
+    
+  // })
+
+
+  // Template.hello.events({
+  //   'click button': function () {
+  //     // increment the counter when button is clicked
+  //     Session.set('counter', Session.get('counter') + 1);
+  //   }
+  // });
+
+  $(document).ready(function(){
+    $('.menu-btn').click(function(){
+        $('nav').toggleClass('open');
+        $(this).toggleClass('close-btn');
+        $('.container').toggle();
+    })
+})
 }
 
 if (Meteor.isServer) {
